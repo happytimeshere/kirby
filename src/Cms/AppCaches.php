1c2ca4e82535f6ac731d92083c7e3adc3fe5aa7a
Lukas Bestle <mail@lukasbestle.com>
2019-04-21 14:24:39 +0200

    Cache: Add NullCache class
    Replaces the previous behavior of the core Cache class, which is now abstract.

--- src/Cms/AppCaches.php
+++ src/Cms/AppCaches.php
@@ -3,6 +3,7 @@
 namespace Kirby\Cms;
 
 use Kirby\Cache\Cache;
+use Kirby\Cache\NullCache;
 use Kirby\Exception\InvalidArgumentException;
 
 trait AppCaches
@@ -25,7 +26,8 @@ trait AppCaches
         $options = $this->cacheOptions($key);
 
         if ($options['active'] === false) {
-            return $this->caches[$key] = new Cache;
+            // use a dummy cache that does nothing
+            return $this->caches[$key] = new NullCache;
         }
 
         $type  = strtolower($options['type']);
